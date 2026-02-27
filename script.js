f76ca49c03a433955a6b80cafe7b5dc32e2b6d2f
daviqueir0z <davibarros.2030@gmail.com>
2026-02-27 00:13:45 -0300

    Refatoração: removendo comentários de estudo

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-// o que é um algoritmo?
-// Um algoritmo é uma sequência de passos ou instruções que são seguidos para realizar uma tarefa ou resolver um problema. 
 
-/* 
-O que vamos fazer? 
-1. colocar os produtos na tela 
-[X]Saber quem são os produtos 
-[X] Onde colocar os produtos na tela 
-    [X]colocar os produtos na tela 
-    [X]Estilizar os produtos 
-
-2. Filtrar por categoria
-[] Saber quem são os botões 
-[]Quando um dos botões foram clicados
-[]Qual botão foi clicado?
-[]Filtrar por produtos daquela categoria
-[]Deixar na tela só os produtos daquela categoria
-
-3. Filtrar por nome 
-[X] saber quem é o input 
-[X] saber quando digitou algo no input 
-[X]  saber o que foi digitado
-[X] Filtrar os produtos de acordo com o que foi digitado
-[X] deixar na tela só os produtos que correspondem ao filtro
-
-*/
 let meuArray = [1, 2, 3, 4, 5,6,7,8,9,10];
 
 
@@ -112,43 +87,30 @@ let meuArray = [1, 2, 3, 4, 5,6,7,8,9,10];
 
 
 
-// Array de produtos
+
 let containerProdutos = document.querySelector(".produtos-container");
-// pra usar uma classe coloca . antes do nome da classe
 
 let inputPesquisa = document.querySelector(".input-pesquisa");
 
 let textoInput = ""
 
 let todosBotoes = document.querySelectorAll(".botao-categorias")
-//  All seria todos 
+
 
 let categoria = "todos"
-//  a variável categoria começa com todos, ou seja, mostra todos os produtos
 
 
-// função para mostrar os produtos na tela
 function mostrarProdutos() {
 
-    // vamos guardar esses produtos em htmlProdutos
     let htmlProdutos = "";
 
-// usaremos a função forEach como uma esteira de produtos trazendo os produtos 
     produtos.forEach(prd=>{
 
-        // includes -> verifica se dois textos batem
-        // prd.nome -> Macbook air -> includes (input) -> Macbook
-//  se o prd.nome tiver a mesma coisa que o que foi digitado no input, ele mostra o produto
-//  o toLocaleLowerCase() serve para deixar tudo em minúsculo e evitar problemas de comparação
 
         if (prd.nome.toLocaleLowerCase().includes(textoInput.toLocaleLowerCase())){
 
       if(prd.categoria == categoria || categoria == "todos"){
-// || significa ou 
-      
 
-        // para mencionar os dados dentro dos objetos, usamos a sintaxe ${} e a cada passada da função  ele vai incluindo os outros objetos
-        // estou guardando o htmlProduto que já tinha + o próximo htmlProduto, se não só ficará visível na página o último produto do loop
       htmlProdutos = htmlProdutos +`
       <div class="cartao-produto">
           <img src="${prd.imagem}" alt="${prd.nome}"  class="imagem-produto"/>
@@ -163,12 +125,11 @@ function mostrarProdutos() {
       }
         }
     })
-    // pega item por item do array, e para cada item cria-se um elemento HTML
-    // o nome prd representa cada produto individualmente, é de sua escolha o nome 
+
 
 
  containerProdutos.innerHTML = htmlProdutos
-    //  depois de terminar o array, informar onde mostrar os produtos no Html e isso usamos .innerHTML
+
 
 }
 mostrarProdutos()
@@ -176,59 +137,34 @@ mostrarProdutos()
 
 
 
-    // função para saber quando digitou algo no input
 
 function pesquisar () {
  textoInput = inputPesquisa.value
 
-    // console.log("digitou algo")
-/*     console.log(inputPesquisa.value)
-    // value é o valor que está dentro do input, só vai mostrar o que foi digitado */
 
-//  colocamos a função mostrarProdutos() para atualizar a lista de produtos toda vez que digitamos algo
     mostrarProdutos()
 }
-// Ferramenta Addeventlistener (ouvinte de eventos) ele serve como um fofoqueiro do código
-//  toda vez que digitar algo no inpput ele vai chamar a função pesquisar 
-//  o js sabe qual input estou me referindo porque criei a varável let inputPesquisa = document.querySelector(".input-pesquisa");
+
 
 inputPesquisa.addEventListener("input", pesquisar)
 
-/* 
-criei uma varável inputPesquisa pra guardar o input
-fiquei de olho no input para saber quando ele foi usado: pegando a variável inputPesquisa que criei e plugo nela o addEventListener
 
-chamei uma função quando ele foi usado: quando digitar algo ele chama a função pesquisar 
-Eu guardei o que foi digitado na variável textoInput o valor de inputPesquisa.value
-
-*/
-
-
-
-// configurando o filtro dos botões 
-
-// Vamos usar o forEach que cria uma matriz e guarda todos os botões [botao,1, botao 2, botao 3 ....]
-//  no forEach nos parenteses precisamos dar um nome botao para nomear a variável, como prd na função mostrar produtos
 todosBotoes.forEach(botao=>{ 
 
-        // vamos usar o addEventListener pra identificar qual botão foi clicado
-        // podemos criar uma função ou chamar a função existente sem precisar escolher nome 
+
  botao.addEventListener("click", function(){
 
 
 
  categoria = botao.getAttribute("data-categoria")
-//  o getAttribute serve para pegar o atributo que está no html, no caso data-categoria
 
 
-// Mudar a cor dos botões quando for clicado 
-// remove a classe ativo de todos os botões
+
 todosBotoes.forEach(b => b.classList.remove("ativo"))
 
-// adiciona a classe ativo ao botão clicado
 botao.classList.add("ativo")
 
-// vamos chamar a função mostrarProdutos() para atualizar a lista de produtos ao clicar no botão
+
 mostrarProdutos()
 })
 
